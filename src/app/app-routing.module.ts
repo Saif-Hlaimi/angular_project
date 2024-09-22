a289f480fdbbf44566d95634261c361222bed604
Saif_Hlaimi <saif.hlaimi@gmail.com>
2024-09-22 16:19:31 +0100

    add the routing navitation first_test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
-const routes: Routes = [{path:'',component:UserComponent}];
+const routes: Routes = [{path:'user',component:UserComponent,pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
