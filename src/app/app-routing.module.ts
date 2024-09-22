198435b5a5b6a9a583cda167c7fd710ad8ad82a1
Saif_Hlaimi <saif.hlaimi@gmail.com>
2024-09-22 16:24:02 +0100

    some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
-const routes: Routes = [{path:'user',component:UserComponent,pathMatch:'full'}];
+const routes: Routes = [{path:'user',component:UserComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
